093ac43a8a3763ffd81186a3c5777a13c139edfb
petarjelenkovic <pera25blok@gmail.com>
2015-04-27 19:06:26 +0200

    Dodate funkcije za open, save, exit i about

--- menjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
+++ menjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
@@ -2,31 +2,49 @@ package menjacnica.gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.TextArea;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+
 import java.awt.Toolkit;
+
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
+
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.BadLocationException;
 import javax.swing.JPopupMenu;
+
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import javax.swing.border.TitledBorder;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+
 import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class MenjacnicaGUI extends JFrame {
 
@@ -53,9 +71,16 @@ public class MenjacnicaGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MenjacnicaGUI() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				izlazak();
+			}
+		});
+		
 		setIconImage(Toolkit.getDefaultToolkit().getImage(MenjacnicaGUI.class.getResource("/resources/theletterm_m012.jpg")));
 		setTitle("Menjacnica");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 650, 477);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -64,29 +89,16 @@ public class MenjacnicaGUI extends JFrame {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Open");
-		mntmNewMenuItem.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/javax/swing/plaf/metal/icons/ocean/directory.gif")));
-		mntmNewMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
-		mnFile.add(mntmNewMenuItem);
 		
-		JMenuItem mntmSave = new JMenuItem("Save");
-		mntmSave.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
-		mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
-		mnFile.add(mntmSave);
 		
-		JMenuItem mntmExit = new JMenuItem("Exit");
-		mntmExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.ALT_MASK));
-		mnFile.add(mntmExit);
 		
-		JMenu mnEdit = new JMenu("Help");
-		menuBar.add(mnEdit);
 		
-		JMenuItem mntmAbout = new JMenuItem("About");
-		mnEdit.add(mntmAbout);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
+		
 		
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, BorderLayout.CENTER);
@@ -134,8 +146,64 @@ public class MenjacnicaGUI extends JFrame {
 		scrollPane_1.setBorder(new TitledBorder(null, "STATUS", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.add(scrollPane_1, BorderLayout.SOUTH);
 		
-		JTextArea textArea = new JTextArea();
+		final JTextArea textArea = new JTextArea();
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
 		scrollPane_1.setViewportView(textArea);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Open");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				JFileChooser fc = new JFileChooser();
+				int rv = fc.showOpenDialog(contentPane);
+				if(rv == JFileChooser.APPROVE_OPTION){
+					File file = fc.getSelectedFile();
+					textArea.append("\n"+"Ucitan fajl: "+file.getAbsolutePath());
+				}
+			}
+		});
+		mntmNewMenuItem.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/javax/swing/plaf/metal/icons/ocean/directory.gif")));
+		mntmNewMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		mnFile.add(mntmNewMenuItem);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mntmSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				JFileChooser fc = new JFileChooser();
+				int rv = fc.showSaveDialog(contentPane);
+				if(rv== JFileChooser.APPROVE_OPTION){
+					File file = fc.getSelectedFile();
+					textArea.append("\n"+"Sacuvan fajl: "+file.getAbsolutePath());
+				}
+			}
+		});
+		mntmSave.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
+		mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		mnFile.add(mntmSave);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				izlazak();
+			}
+		});
+		mntmExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.ALT_MASK));
+		mnFile.add(mntmExit);
+		
+		JMenu mnEdit = new JMenu("Help");
+		menuBar.add(mnEdit);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(contentPane, "Author: Petar Jelenkovic 105/13", "About",JOptionPane.INFORMATION_MESSAGE);
+						
+			}
+		});
+		mnEdit.add(mntmAbout);
+		
 		
 		JPanel panel = new JPanel();
 		panel.setPreferredSize(new Dimension(130, 10));
@@ -172,4 +240,13 @@ public class MenjacnicaGUI extends JFrame {
 			}
 		});
 	}
+	
+	private  void izlazak(){
+		int a = JOptionPane.showConfirmDialog(contentPane, "Da li zelite da izadjete iz programa?", "Upozorenje", JOptionPane.YES_NO_CANCEL_OPTION);
+		if(a == JOptionPane.YES_OPTION){
+			System.exit(0);
+		}
+	}
+	
+	
 }
